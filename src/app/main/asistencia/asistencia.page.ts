467abd03e93f28cfd7284743c7bc41654a9e186a
abdeel94 <abdeel4991@hotmail.com>
2022-08-31 01:45:30 -0400

    lista ramos

--- src/app/main/asistencia/asistencia.page.ts
+++ src/app/main/asistencia/asistencia.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-asistencia',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsistenciaPage implements OnInit {
 
+
   constructor() { }
+  
 
   ngOnInit() {
   }
+
+  
+  ramos:any[]=[
+    {id:101,nombre:"Matemáticas",clasesRealizadas:12,clasesAsistidas:11},
+    {id:102,nombre:"Programación",clasesRealizadas:10,clasesAsistidas:10},
+    {id:103,nombre:"Inglés",clasesRealizadas:16,clasesAsistidas:12},
+    {id:104,nombre:"Comunicación",clasesRealizadas:8,clasesAsistidas:2},
+    {id:105,nombre:"Base de datos",clasesRealizadas:8,clasesAsistidas:7},
+    {id:106,nombre:"Ética",clasesRealizadas:10,clasesAsistidas:5},
+  ]
 
 }
