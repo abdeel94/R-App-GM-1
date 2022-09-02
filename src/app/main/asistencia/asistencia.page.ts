ad2c505edba2f3762b6a934007cc42c4f6876919
abdeel94 <abdeel4991@hotmail.com>
2022-09-01 20:29:58 -0400

    theming y pipe percent

--- src/app/main/asistencia/asistencia.page.ts
+++ src/app/main/asistencia/asistencia.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import {formatPercent} from '@angular/common';
 
 @Component({
   selector: 'app-asistencia',
@@ -9,20 +9,20 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class AsistenciaPage implements OnInit {
 
 
-  constructor() { }
-  
+  constructor(){}
+
 
   ngOnInit() {
   }
 
-  
-  ramos:any[]=[
-    {id:101,nombre:"Matemáticas",clasesRealizadas:12,clasesAsistidas:11},
-    {id:102,nombre:"Programación",clasesRealizadas:10,clasesAsistidas:10},
-    {id:103,nombre:"Inglés",clasesRealizadas:16,clasesAsistidas:12},
-    {id:104,nombre:"Comunicación",clasesRealizadas:8,clasesAsistidas:2},
-    {id:105,nombre:"Base de datos",clasesRealizadas:8,clasesAsistidas:7},
-    {id:106,nombre:"Ética",clasesRealizadas:10,clasesAsistidas:5},
+
+  ramos: any[] = [
+    { id: 101, nombre: "Matemáticas", clasesRealizadas: 12, clasesAsistidas: 11},
+    { id: 102, nombre: "Programación", clasesRealizadas: 10, clasesAsistidas: 10},
+    { id: 103, nombre: "Inglés", clasesRealizadas: 16, clasesAsistidas: 12},
+    { id: 104, nombre: "Comunicación", clasesRealizadas: 8, clasesAsistidas: 2},
+    { id: 105, nombre: "Base de datos", clasesRealizadas: 8, clasesAsistidas: 7},
+    { id: 106, nombre: "Ética", clasesRealizadas: 10, clasesAsistidas: 5},
   ]
 
 }
